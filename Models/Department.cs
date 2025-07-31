a13706ea2dcbce78e93421ad5a737173e06a536f
skuma <skuma@SHANKY>
2025-07-31 18:37:35 +0530

    Add project files.

---
+++ Models/Department.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Department
+    {
+        //One to Many Relationship
+        public int DepartmentId { get; set; } // PrimaryKey
+        public string Name { get; set; }
+        
+        //Relationship with Employees
+        public List<Employee> Employees { get; set; }
+    }
+}
